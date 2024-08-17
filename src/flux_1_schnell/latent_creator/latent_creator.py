8e16eeb6a0ed800909eb2f3ad5f254646ee46b18
Fabio <f.xuzzo@gmail.com>
2024-08-17 22:50:35 +0200

    change naming

--- src/flux_1_schnell/latent_creator/latent_creator.py
+++ src/flux_1_schnell/latent_creator/latent_creator.py
@@ -5,7 +5,7 @@ class LatentCreator:
 
     @staticmethod
     def create(height: int, width: int, seed: int) -> (mx.array, mx.array):
-        r_h = height // 16
-        r_w = width // 16
-        latents = mx.random.normal(shape=[1, r_h * r_w, 64], key=mx.random.key(seed))
+        latent_height = height // 16
+        latent_width = width // 16
+        latents = mx.random.normal(shape=[1, latent_height * latent_width, 64], key=mx.random.key(seed))
         return latents
