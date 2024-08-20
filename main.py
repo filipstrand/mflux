ef4e482864474debd64f14c72dd7c2ead41fec89
filipstrand <strand.filip@gmail.com>
2024-08-20 09:32:05 +0200

    Add separate model config and other small updates

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from flux_1_schnell.config.config import Config
 from flux_1_schnell.flux import Flux1
 from flux_1_schnell.post_processing.image_util import ImageUtil
 
+
 def main():
     parser = argparse.ArgumentParser(description='Generate an image based on a prompt.')
     parser.add_argument('--prompt', type=str, required=True, help='The textual description of the image to generate.')
     parser.add_argument('--output', type=str, default="image.png", help='The filename for the output image. Default is "image.png".')
-    parser.add_argument('--model', type=str, default="black-forest-labs/FLUX.1-schnell", help='The model to use. Default is "black-forest-labs/FLUX.1-schnell".')
-    parser.add_argument('--max_sequence_length', type=int, default=256, help='Max Sequence Length (Default is 256)')
+    parser.add_argument('--model', type=str, default="schnell", help='The model to use. Default is "schnell".')
     parser.add_argument('--seed', type=int, default=None, help='Entropy Seed (Default is time-based random-seed)')
     parser.add_argument('--height', type=int, default=1024, help='Image height (Default is 1024)')
     parser.add_argument('--width', type=int, default=1024, help='Image width (Default is 1024)')
@@ -25,7 +25,7 @@ def main():
 
     seed = int(time.time()) if args.seed is None else args.seed
 
-    flux = Flux1(args.model, max_sequence_length=args.max_sequence_length)
+    flux = Flux1.from_alias(args.model)
 
     image = flux.generate_image(
         seed=seed,
