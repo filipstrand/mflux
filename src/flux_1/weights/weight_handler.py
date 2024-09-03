c9cc0ee29d2a6c0cf46bf597a72eb68a031d966b
Sujip Maharjan <elitexp2008@gmail.com>
2024-09-03 07:38:04 +0545

    Apply lora with --apply-lora <path-to-safetensors>

--- src/flux_1/weights/weight_handler.py
+++ src/flux_1/weights/weight_handler.py
@@ -6,14 +6,23 @@ from mlx.utils import tree_unflatten
 from safetensors import safe_open
 
 from flux_1.config.config import Config
+import icecream as ic
+import json
+from mlx.utils import tree_flatten
+from functools import reduce
 
-
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NumpyEncoder, self).default(obj)
 class WeightHandler:
 
     def __init__(
             self,
             repo_id: str | None = None,
             local_path: str | None = None,
+            lora_path: str | None = None
     ):
         root_path = Path(local_path) if local_path else WeightHandler._download_or_get_cached_weights(repo_id)
 
@@ -21,6 +30,77 @@ class WeightHandler:
         self.t5_encoder, _ = WeightHandler._t5_encoder(root_path=root_path)
         self.vae, _ = WeightHandler._vae(root_path=root_path)
         self.transformer, self.quantization_level = WeightHandler._transformer(root_path=root_path)
+        if(lora_path is not None):
+            self.lora_transformer,self.lora_quantization_level= WeightHandler._lora_transformer(lora_path=lora_path)
+            if 'transformer' not in self.lora_transformer:
+                pass
+            self._apply_transformer(self.transformer,self.lora_transformer['transformer'])
+        
+    def _apply_transformer(self,transformer,lora_transformer):
+        lora_weights = tree_flatten(lora_transformer)
+        visited={}
+        
+        for key,weight in lora_weights:
+            splits=key.split(".")
+            target=transformer
+            visiting=[]
+            for splitKey in splits:
+                if isinstance(target,dict) and splitKey in target:
+                    target=target[splitKey]
+                    visiting.append(splitKey)
+                elif isinstance(target,list) and len(target)>0:
+                    if(len(target)< int(splitKey)):
+                        for _ in range(int(splitKey)-len(target)+1):
+                            target.append({})
+                        
+                    target=target[int(splitKey)]
+                    visiting.append(splitKey)
+                else:
+                    parentKey=".".join(visiting)
+                    if(parentKey in visited and 'lora_A' in visited[parentKey] and 'lora_B' in visited[parentKey]):
+                        continue
+                    if not splitKey.startswith("lora_"):
+                        visiting.append(splitKey)
+                        parentKey=".".join(visiting)
+                        if(splitKey=="net"):
+                            target['net']=list({})
+                            target=target['net']
+                        elif (splitKey=="0"):
+                            target.append({})
+                            target=target[0]
+                            continue
+                        elif (splitKey=="proj"):
+                            target[splitKey]=weight
+                            if parentKey not in visited:
+                                visited[parentKey]={}
+                        continue
+                    if parentKey not in visited:
+                        visited[parentKey]={}
+                    visited[parentKey][splitKey]=weight
+                    if not 'weight' in target:
+                        continue
+                    if  'lora_A' in visited[parentKey] and 'lora_B' in visited[parentKey]:
+                        lora_a=visited[parentKey]['lora_A']
+                        lora_b=visited[parentKey]['lora_B']
+                        transWeight=target['weight']
+                        weight=transWeight + lora_b @lora_a
+                        target['weight']=weight
+                        
+                        
+
+    
+
+    @staticmethod
+    def _lora_transformer(lora_path: Path) -> (dict, int):
+        weights = []
+        quantization_level = safe_open(lora_path, framework="pt").metadata().get("quantization_level")
+        weight = list(mx.load(str(lora_path)).items())
+        weights.extend(weight)
+        weights = [WeightHandler._reshape_weights(k, v) for k, v in weights]
+        weights = WeightHandler._flatten(weights)
+        unflatten = tree_unflatten(weights)
+        return unflatten, quantization_level
+
 
     @staticmethod
     def _clip_encoder(root_path: Path) -> (dict, int):
@@ -74,6 +154,8 @@ class WeightHandler:
                     "linear2": block["ff_context"]["net"][2]
                 }
         return weights, quantization_level
+    
+    
 
     @staticmethod
     def _vae(root_path: Path) -> (dict, int):
