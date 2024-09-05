96848e320ffafe5a5b4b1d86c3edcf3a4582afac
filipstrand <strand.filip@gmail.com>
2024-09-05 19:37:50 +0200

    Add support for multiple LoRAs again

--- src/flux_1/flux.py
+++ src/flux_1/flux.py
@@ -28,8 +28,8 @@ class Flux1:
             model_config: ModelConfig,
             quantize_full_weights: int | None = None,
             local_path: str | None = None,
-            lora_path: str | None = None,
-            lora_scale: float = 1.0,
+            lora_paths: list[str] | None = None,
+            lora_scales: list[float] | None = None,
     ):
         self.model_config = model_config
         self.quantize_full_weights = quantize_full_weights
@@ -46,7 +46,7 @@ class Flux1:
         self.clip_text_encoder = CLIPEncoder()
 
         # Load the weights from disk, huggingface cache, or download from huggingface
-        weights = WeightHandler(repo_id=model_config.model_name, local_path=local_path, lora_path=lora_path, lora_scale=lora_scale)
+        weights = WeightHandler(repo_id=model_config.model_name, local_path=local_path, lora_paths=lora_paths, lora_scales=lora_scales)
 
         # Set the loaded weights if they are not quantized
         if weights.quantization_level is None:
