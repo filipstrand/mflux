2e4046d38520e897fc799ea9b656f068ad294f89
filipstrand <strand.filip@gmail.com>
2024-09-06 20:54:10 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mflux",
+    version="0.2.0",
+    author="Filip Strand",
+    author_email="strand.filip@gmail.com",
+    description="A MLX port of FLUX based on the Huggingface Diffusers implementation.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/filipstrand/mflux",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS",
+    ],
+    python_requires='>=3.11',
+    install_requires=[
+        'mlx>=0.16.0',
+        'numpy>=2.0.0',
+        'pillow>=10.4.0',
+        'transformers>=4.44.0',
+        'sentencepiece>=0.2.0',
+        'torch>=2.3.1',
+        'tqdm>=4.66.5',
+        'huggingface-hub>=0.24.5',
+        'safetensors>=0.4.4',
+        'piexif>=1.1.3',
+    ],
+)
