d6d05e9a753838df026fe3d40c3fc7f6a10e4405
Anthony Wu <462072+anthonywu@users.noreply.github.com>
2024-10-11 15:03:52 -0700

    refactor argparser

---
+++ src/mflux/ui/defaults.py
@@ -0,0 +1,9 @@
+CONTROLNET_STRENGTH = 0.4
+GUIDANCE_SCALE = 3.5
+HEIGHT, WIDTH = 1024, 1024
+MODEL_CHOICES = ["dev", "schnell"]
+MODEL_INFERENCE_STEPS = {
+    "dev": 14,
+    "schnell": 4,
+}
+QUANTIZE_CHOICES = [4, 8]
