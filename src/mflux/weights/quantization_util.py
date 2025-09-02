d409ccfc6c112cc41ab147ce5339f6728e00f38d
Anthony Wu <pls-file-gh-issue@users.noreply.github.com>
2025-09-02 14:16:02 -0700

    Fix: loading non-quantized saved models

--- src/mflux/weights/quantization_util.py
+++ src/mflux/weights/quantization_util.py
@@ -18,6 +18,14 @@ class QuantizationUtil:
         weights: "WeightHandler",
     ) -> None:
         q_level = weights.meta_data.quantization_level
+        breakpoint()
+
+        # mx.save_tensors saves metadata dict kv 'quantization_level': 'None' as a str: str mapping
+        # we coerce both configs to NoneType to help users use non-quantized saved model files
+        if q_level == "None":
+            q_level = None
+        if quantize == "None":
+            quantize = None
 
         if quantize is not None or q_level is not None:
             bits = int(q_level) if q_level is not None else quantize
