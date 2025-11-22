02006c44a71ab62caebd0941a04e9de6493cbbb7
filipstrand <strand.filip@gmail.com>
2025-11-22 13:10:28 +0100

    refactor...

--- src/mflux/models/fibo/weights/fibo_weight_mapping.py
+++ src/mflux/models/fibo/weights/fibo_weight_mapping.py
@@ -1,26 +1,11 @@
 from typing import List
 
-import mlx.core as mx
-
 from mflux.models.common.weights.mapping.weight_mapping import WeightMapping, WeightTarget
-
-
-def reshape_gamma_to_1d(tensor: mx.array) -> mx.array:
-    if len(tensor.shape) > 1:
-        return mx.reshape(tensor, (tensor.shape[0],))
-    return tensor
-
-
-def transpose_conv3d_weight(tensor: mx.array) -> mx.array:
-    if len(tensor.shape) == 5:
-        return tensor.transpose(0, 2, 3, 4, 1)
-    return tensor
-
-
-def transpose_conv2d_weight(tensor: mx.array) -> mx.array:
-    if len(tensor.shape) == 4:
-        return tensor.transpose(0, 2, 3, 1)
-    return tensor
+from mflux.models.qwen.weights.qwen_weight_mapping import (
+    reshape_gamma_to_1d,
+    transpose_conv2d_weight,
+    transpose_conv3d_weight,
+)
 
 
 class FIBOWeightMapping(WeightMapping):
